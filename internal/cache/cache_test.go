6b08ac0d453741014c58c416acd51beef2743c3c
Oleg <3680312+ximura@users.noreply.github.com>
2024-12-01 13:21:36 +0200

    feat: cache with simple mutex

--- internal/cache/cache_test.go
+++ internal/cache/cache_test.go
@@ -1,6 +1,7 @@
 package cache_test
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/ximura/sleipnir/internal/cache"
@@ -20,4 +21,40 @@ func TestGetAfterSet(t *testing.T) {
 	v, ok := c.Get("foo")
 	assert.Equal(t, ok, true, "Got false for existing item")
 	assert.Equal(t, v, 1, "Got wrong value for existing item")
+}
+
+func TestShouldOverwriteExistingValue(t *testing.T) {
+	c := cache.New[string, int]()
+	c.Set("foo", 1)
+	v, ok := c.Get("foo")
+	assert.Equal(t, ok, true, "Got false for existing item")
+	assert.Equal(t, v, 1, "Got wrong value for existing item")
+
+	c.Set("foo", 2)
+	v, ok = c.Get("foo")
+	assert.Equal(t, ok, true, "Got false for existing item")
+	assert.Equal(t, v, 2, "Got wrong value for existing item")
+}
+
+func TestParallelSet(t *testing.T) {
+	n := 10
+	c := cache.New[string, int]()
+
+	var wg sync.WaitGroup
+	wg.Add(n)
+	for i := range n {
+		go func(i int) {
+			c.Set("foo", i)
+			c.Set("bar", i)
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+
+	_, ok1 := c.Get("foo")
+	_, ok2 := c.Get("bar")
+
+	assert.Equal(t, ok1, true, "Got false for existing item")
+	assert.Equal(t, ok2, true, "Got false for existing item")
 }
